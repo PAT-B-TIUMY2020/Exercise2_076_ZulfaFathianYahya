5965315a50ad781ba25f679da8bcbad6ae609575
fafathian <56532114+fafathian@users.noreply.github.com>
2020-12-25 13:29:19 +0700

    Add project files.

---
+++ Server_076_ZulfaFathianyahya/Form1.cs
@@ -0,0 +1,67 @@
+﻿using Service_076_ZulfaFathianYahya;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server_076_ZulfaFathianyahya
+{
+    public partial class Form1 : Form
+    {
+        ServiceHost hostObject;
+        public Form1()
+        {
+            InitializeComponent();
+            button2.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            hostObject = null;
+
+            try
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    hostObject = new ServiceHost(typeof(TI_UMY));
+                    hostObject.Open();
+                });
+                label3.Text = "Sudah Berjalan";
+                label4.Text = "Klik OFF untuk menonaktifkan server";
+                button1.Enabled = false;
+                button2.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                hostObject = null;
+                label2.Text = "Server Error";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                hostObject.Abort();
+                label3.Text = "Sudah dimatikan";
+                label4.Text = "Klik ON untuk menghidupkan server";
+                button2.Enabled = false;
+                button1.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                button1.Enabled = false;
+                button2.Enabled = true;
+                label2.Text = "Server Error";
+            }
+        }
+    }
+}
